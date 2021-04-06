b1352be29ea7505bfab7a0edb92d09e68e6bdcf7
josephsdavid <josephsd@smu.edu>
2021-04-06 14:00:43 -0500

    cleaned up

--- create_results.py
+++ create_results.py
@@ -1,9 +1,7 @@
-import jplspec as jpl
+import autospec
 from scipy import stats as ss
-from pprint import pprint
 import numpy as np
 import sys
-from itertools import chain
 import os
 
 stdout = sys.stdout
@@ -18,17 +16,17 @@ files = [f"{PATH}{f}" for f in os.listdir(PATH) if f == fe]
 
 for f in files:
     os.system(f"notify-send 'started!' {f}")
-    data = jpl.read(jpl.SpectralFile(f))
+    data = autospec.read(autospec.SpectralFile(f))
     data = data._asdict()
     data['frequency'] = data['frequency'][np.where((data['frequency'] > 255.8) & (data['frequency']< 256.08)) ]
     data['intensity'] = data['intensity'][np.where((data['frequency'] > 255.8) & (data['frequency']< 256.08)) ]
-    data2 = jpl.SpectralData(**data)
-    spikes = jpl.identify_spikes(data2, std_method=lambda x: ss.trimboth(x, 0.1).std())
-    jpl.plot_spikes(spikes)
+    data2 = autospec.SpectralData(**data)
+    spikes = autospec.identify_spikes(data2, std_method=lambda x: ss.trimboth(x, 0.1).std())
+    autospec.plot_spikes(spikes)
 #    import pdb; pdb.set_trace()
     save_path = f.split("/")[-1].replace(".", "_").replace("_txt","") + "working_example_zoom2"
-    molecules = jpl.get_molecules_from_spikes(spikes, f"{save_path}.obj")
-    cover = jpl.SetCovering(spikes, molecules, threshold=0.1)
+    molecules = autospec.get_molecules_from_spikes(spikes, f"{save_path}.obj")
+    cover = autospec.SetCovering(spikes, molecules, threshold=0.1)
     cover.save_results(save_path)
     os.system("notify-send 'done!'")
 
